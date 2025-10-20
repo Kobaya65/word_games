32c4e63c5b787d86ddfcc15a69f366e355ae2153
Kobaya65 <kobaya65@free.fr>
2025-10-20 17:25:18 +0200

    Refactored following SonarQube advices

--- le_pendu.py
+++ le_pendu.py
@@ -1,6 +1,6 @@
 """The game of hangman"""
 
-import string
+from string import ascii_lowercase
 
 class bcolors:
     """Color codes."""
@@ -18,7 +18,7 @@ class bcolors:
     CURSOR_UP_15_LINES = '\033[15A'
 
 
-def main() -> None:
+def hangman() -> None:
     """Main function."""
     lettres_manquees = set()
     clean_terminal()
@@ -36,35 +36,10 @@ def main() -> None:
         lettre = input(f'Tour {tour + 1}. Entrez une lettre : ').lower()
         tour += 1
         # on regarde si la lettre saisie fait partie de l'alphabet ou des caractères accentués
-        if lettre not in  string.ascii_lowercase + 'àäâéèëêîïöôûüç':
+        if lettre not in  ascii_lowercase + 'àäâéèëêîïöôûüç':
             print(f'{lettre} n\'est pas une lettre.')
         else:
-            if lettre in lettres_manquees or lettre in mot_decouvert:
-                print(f"Lettre '{lettre}' déjà proposée.")
-                tour -= 1
-            else:
-                trouve = False
-                for idx, l in enumerate(mot):
-                    if l == lettre:
-                        trouve = True
-                        # recomposer mot_decouvert
-                        mot_decouvert = f'{mot_decouvert[:idx]}{lettre}{mot_decouvert[idx + 1:]}'
-
-                if not trouve:
-                    # si lettre non trouvé dans mot, on l'ajoute dans le set
-                    lettres_manquees.add(lettre)
-                    nb_erreurs -= 1
-
-                clean_terminal()
-                if lettres_manquees:
-                    lettres_proposees = ''
-                    # composer la liste des lettres manquées pour affichage
-                    for l in lettres_manquees:
-                        lettres_proposees += f'{l}, '
-                    lettres_proposees = lettres_proposees[:-2]
-                    print(f'Autres lettres proposées : {lettres_proposees}')
-
-                print(f'Mot à découvrir : {mot_decouvert}')
+            mot_decouvert, tour, nb_erreurs = check_letter(lettres_manquees, mot, mot_decouvert, lettre, nb_erreurs, tour)
 
     if mot == mot_decouvert:
         print(f'Bravo, vous avez gagné en {tour} coup{'s' if tour > 1 else ''} !')
@@ -72,6 +47,51 @@ def main() -> None:
         print('Vous avez perdu...')
         print(f'Votre essai : {mot_decouvert}.')
         print(f'Le mot à découvrir était : {mot}.')
+
+def check_letter(lettres_manquees: set, mot: str, mot_decouvert: str, lettre: str, nb_erreurs: int, tour: int) -> tuple[str, int, int]:
+    """_summary_
+
+    Args:
+        lettres_manquees (set): wrong letters already proposed
+        mot (str): word to be discovered
+        mot_decouvert (str): word with already discovered letters
+        lettre (str): proposed letter
+        nb_erreurs (int): number of errors done
+        tour (int): turn number
+
+    Returns:
+        mot_decouvert (str): word with already discovered letters
+        tour (int): turn number
+        nb_erreurs (int): number of errors done
+    """
+    if lettre in lettres_manquees or lettre in mot_decouvert:
+        print(f"Lettre '{lettre}' déjà proposée.")
+        tour -= 1
+    else:
+        trouve = False
+        for idx, l in enumerate(mot):
+            if l == lettre:
+                trouve = True
+                        # recomposer mot_decouvert
+                mot_decouvert = f'{mot_decouvert[:idx]}{lettre}{mot_decouvert[idx + 1:]}'
+
+        if not trouve:
+                    # si lettre non trouvé dans mot, on l'ajoute dans le set
+            lettres_manquees.add(lettre)
+            nb_erreurs -= 1
+
+        clean_terminal()
+        if lettres_manquees:
+            lettres_proposees = ''
+                    # composer la liste des lettres manquées pour affichage
+            for l in lettres_manquees:
+                lettres_proposees += f'{l}, '
+            lettres_proposees = lettres_proposees[:-2]
+            print(f'Autres lettres proposées : {lettres_proposees}')
+
+        print(f'Mot à découvrir : {mot_decouvert}')
+
+    return mot_decouvert, tour, nb_erreurs
 
 
 def clean_terminal() -> None:
@@ -81,4 +101,4 @@ def clean_terminal() -> None:
 
 
 if __name__ == '__main__':
-    main()
+    hangman()
